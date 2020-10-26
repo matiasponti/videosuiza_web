0a372041aa6d0aeff1f5080444a6598c9f17e146
matiasponti <mponti@miiii.app>
2020-10-25 22:11:27 -0300

    adelanto landing

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
+import { FaBars } from 'react-icons/fa';
+
 
 const Navbar = () => {
-    return ( 
+    return (
         <>
-        
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">
+                        Video Suiza
+            </NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="discover">Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="services">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="contact">Contact</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="signup">Sign Up</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
         </>
-     );
+    );
 }
- 
+
 export default Navbar;
